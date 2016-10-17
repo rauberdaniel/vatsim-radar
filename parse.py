fc69dcfaa76ed3de9567be1f14e52accc2e2bc34
Daniel Rauber <mail@danielrauber.de>
2016-10-17 17:04:36 +1300

    added popups to airplanes

--- parse.py
+++ parse.py
@@ -5,6 +5,7 @@ import json
 import sched, time
 from datetime import datetime
 import os
+import sys
 
 dataUrl = "http://info.vroute.net/vatsim-data.txt"
 clientInfo = ["callsign","cid","realname","clienttype","frequency","latitude","longitude","altitude","groundspeed","planned_aircraft","planned_tascruise","planned_depairport","planned_altitude","planned_destairport","server","protrevision","rating","transponder","facilitytype","visualrange","planned_revision","planned_flighttype","planned_deptime","planned_actdeptime","planned_hrsenroute","planned_minenroute","planned_hrsfuel","planned_minfuel","planned_altairport","planned_remarks","planned_route","planned_depairport_lat","planned_depairport_lon","planned_destairport_lat","planned_destairport_lon","atis_message","time_last_atis_received","time_logon","heading","QNH_iHg","QNH_Mb"]
@@ -54,7 +55,8 @@ def update():
   jsonFile.close()
 
   print('done.')
-  s.enter(60, 1, update)
+  if("--single" not in sys.argv):
+    s.enter(60, 1, update)
 
 
 s.enter(1, 1, update)
